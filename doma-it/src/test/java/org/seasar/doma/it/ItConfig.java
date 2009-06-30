b48ada477ec79e4fdf73a97069aeb4bb1d8c2320
nakamura-to <toshihiro.nakamura@gmail.com>
2009-06-30 09:35:56 +0000

    ヘッダーにコピーライトをつけました。 maven化しました（でもビルドできない。。。aptで生成されたクラスのパッケージと利用側のクラスのパッケージがちがうから？）。
    git-svn-id: https://www.seasar.org/svn/doma/trunk@12 362887e2-e868-44d1-a061-b03550a0b845

--- doma-it/src/test/java/org/seasar/doma/it/ItConfig.java
+++ doma-it/src/test/java/org/seasar/doma/it/ItConfig.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2009 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.seasar.doma.it;
 
 import javax.sql.DataSource;
@@ -12,7 +27,7 @@ public class ItConfig extends DomaAbstractConfig {
 
     protected static final JdbcLogger logger = new ItLogger();
 
-    protected static final S2RequiresNewController controller = new S2RequiresNewController();
+    protected static final RequiresNewController controller = new S2RequiresNewController();
 
     @Override
     public DataSource dataSource() {
--- doma-it/src/test/java/org/seasar/doma/it/S2RequiresNewController.java
+++ doma-it/src/test/java/org/seasar/doma/it/S2RequiresNewController.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2009 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.seasar.doma.it;
 
 import org.seasar.doma.jdbc.RequiresNewController;
@@ -12,7 +27,7 @@ public class S2RequiresNewController implements RequiresNewController {
     public <R> R requiresNew(final Callback<R> callback) throws Throwable {
         final TransactionManagerAdapter txAdapter = SingletonS2Container
                 .getComponent(TransactionManagerAdapter.class);
-        final Object result = txAdapter.requiresNew(new TransactionCallback() {
+        Object result = txAdapter.requiresNew(new TransactionCallback() {
 
             public Object execute(final TransactionManagerAdapter adapter)
                     throws Throwable {
