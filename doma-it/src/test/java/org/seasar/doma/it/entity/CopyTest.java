f1d890daf58c09e9eac10b0c4d016fbbbefda5e5
nakamura-to <toshihiro.nakamura@gmail.com>
2009-09-03 09:55:57 +0000

    itのテストが通らないので調整。
    git-svn-id: https://www.seasar.org/svn/doma/trunk@171 362887e2-e868-44d1-a061-b03550a0b845

--- doma-it/src/test/java/org/seasar/doma/it/entity/CopyTest.java
+++ doma-it/src/test/java/org/seasar/doma/it/entity/CopyTest.java
@@ -7,7 +7,6 @@ import java.sql.Timestamp;
 
 import org.junit.runner.RunWith;
 import org.seasar.doma.copy.CopyUtil;
-import org.seasar.doma.it.dto.EmpDto;
 import org.seasar.framework.unit.Seasar2;
 import org.seasar.framework.unit.annotation.TxBehavior;
 import org.seasar.framework.unit.annotation.TxBehaviorType;
@@ -61,4 +60,89 @@ public class CopyTest {
                 .updateTimestamp().get());
     }
 
+    public static class EmpDto implements java.io.Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+        private java.lang.Integer id;
+
+        private java.sql.Timestamp insertTimestamp;
+
+        private java.lang.String name;
+
+        private java.math.BigDecimal salary;
+
+        private java.lang.String temp;
+
+        private java.util.List<java.lang.String> tempList = new java.util.ArrayList<java.lang.String>();
+
+        private java.sql.Timestamp updateTimestamp;
+
+        private java.lang.Integer version;
+
+        public java.lang.Integer getId() {
+            return id;
+        }
+
+        public void setId(java.lang.Integer id) {
+            this.id = id;
+        }
+
+        public java.sql.Timestamp getInsertTimestamp() {
+            return insertTimestamp;
+        }
+
+        public void setInsertTimestamp(java.sql.Timestamp insertTimestamp) {
+            this.insertTimestamp = insertTimestamp;
+        }
+
+        public java.lang.String getName() {
+            return name;
+        }
+
+        public void setName(java.lang.String name) {
+            this.name = name;
+        }
+
+        public java.math.BigDecimal getSalary() {
+            return salary;
+        }
+
+        public void setSalary(java.math.BigDecimal salary) {
+            this.salary = salary;
+        }
+
+        public java.lang.String getTemp() {
+            return temp;
+        }
+
+        public void setTemp(java.lang.String temp) {
+            this.temp = temp;
+        }
+
+        public java.util.List<java.lang.String> getTempList() {
+            return tempList;
+        }
+
+        public void setTempList(java.util.List<java.lang.String> tempList) {
+            this.tempList = tempList;
+        }
+
+        public java.sql.Timestamp getUpdateTimestamp() {
+            return updateTimestamp;
+        }
+
+        public void setUpdateTimestamp(java.sql.Timestamp updateTimestamp) {
+            this.updateTimestamp = updateTimestamp;
+        }
+
+        public java.lang.Integer getVersion() {
+            return version;
+        }
+
+        public void setVersion(java.lang.Integer version) {
+            this.version = version;
+        }
+
+    }
 }
