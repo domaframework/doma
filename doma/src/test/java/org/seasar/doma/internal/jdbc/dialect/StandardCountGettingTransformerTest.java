70b4ec7ad42004e8adae3efd17b05b63415d6883
nakamura-to <toshihiro.nakamura@gmail.com>
2009-11-13 04:02:29 +0000

    [DOMA-62][Core] 検索時にselect count(*) を同時に実行する機能を実装しました、でselect count(*) のサブクエリで必ずエイリアスを持つようにしました（PostgreSQLでこけるので）。
    git-svn-id: https://www.seasar.org/svn/doma/trunk@425 362887e2-e868-44d1-a061-b03550a0b845

--- doma/src/test/java/org/seasar/doma/internal/jdbc/dialect/StandardCountGettingTransformerTest.java
+++ doma/src/test/java/org/seasar/doma/internal/jdbc/dialect/StandardCountGettingTransformerTest.java
@@ -30,7 +30,7 @@ import org.seasar.doma.jdbc.SqlNode;
 public class StandardCountGettingTransformerTest extends TestCase {
 
     public void test() throws Exception {
-        String expected = "select count(*) from ( select * from emp)";
+        String expected = "select count(*) from ( select * from emp) t_";
         StandardCountGettingTransformer transformer = new StandardCountGettingTransformer();
         SqlParser parser = new SqlParser("select * from emp");
         SqlNode sqlNode = transformer.transform(parser.parse());
@@ -41,7 +41,7 @@ public class StandardCountGettingTransformerTest extends TestCase {
     }
 
     public void testOrderBy() throws Exception {
-        String expected = "select count(*) from ( select * from emp )";
+        String expected = "select count(*) from ( select * from emp ) t_";
         StandardCountGettingTransformer transformer = new StandardCountGettingTransformer();
         SqlParser parser = new SqlParser("select * from emp order by emp.id");
         SqlNode sqlNode = transformer.transform(parser.parse());
