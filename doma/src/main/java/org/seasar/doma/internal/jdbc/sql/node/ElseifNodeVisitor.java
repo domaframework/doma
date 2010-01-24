64702c151c0bb1f17d1ad9435e486958d7b4dfbc
nakamura-to <toshihiro.nakamura@gmail.com>
2010-01-24 06:28:10 +0000

    コピーライトを修正しました。
    git-svn-id: https://www.seasar.org/svn/doma/trunk@616 362887e2-e868-44d1-a061-b03550a0b845

--- doma/src/main/java/org/seasar/doma/internal/jdbc/sql/node/ElseifNodeVisitor.java
+++ doma/src/main/java/org/seasar/doma/internal/jdbc/sql/node/ElseifNodeVisitor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2009 the Seasar Foundation and the Others.
+ * Copyright 2004-2010 the Seasar Foundation and the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
