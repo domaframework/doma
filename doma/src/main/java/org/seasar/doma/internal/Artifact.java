c8bb3922c01ce3759ec332621a5af7c0e366346b
nakamura-to <toshihiro.nakamura@gmail.com>
2010-02-01 16:32:38 +0000

    1.0.0開発開始。 Aptinaのバージョンを更新。
    git-svn-id: https://www.seasar.org/svn/doma/trunk@646 362887e2-e868-44d1-a061-b03550a0b845

--- doma/src/main/java/org/seasar/doma/internal/Artifact.java
+++ doma/src/main/java/org/seasar/doma/internal/Artifact.java
@@ -24,7 +24,7 @@ public final class Artifact {
 
     private static final String NAME = "Doma";
 
-    private static final String VERSION = "1.0.0-RC3";
+    private static final String VERSION = "1.0.0-SNAPSHOT";
 
     public static String getName() {
         return NAME;
