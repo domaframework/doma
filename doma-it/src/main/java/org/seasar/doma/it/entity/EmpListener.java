2f73ce461acdcec94e15ee118773b37b16f71724
nakamura-to <toshihiro.nakamura@gmail.com>
2010-12-26 13:50:58 +0000

    [DOMA-211][Core] EntityListenerでpostInsert、postUpdate、postDeleteメソッドをサポートしました。 [DOMA-210][Core] EntityListenerのpreUpdateメソッドでエンティティに変更があったことを検出できるようにしました。
    git-svn-id: https://www.seasar.org/svn/doma/trunk@935 362887e2-e868-44d1-a061-b03550a0b845

--- doma-it/src/main/java/org/seasar/doma/it/entity/EmpListener.java
+++ doma-it/src/main/java/org/seasar/doma/it/entity/EmpListener.java
@@ -19,21 +19,41 @@ import java.sql.Timestamp;
 import java.util.Date;
 
 import org.seasar.doma.jdbc.entity.EntityListener;
+import org.seasar.doma.jdbc.entity.PostDeleteContext;
+import org.seasar.doma.jdbc.entity.PostInsertContext;
+import org.seasar.doma.jdbc.entity.PostUpdateContext;
+import org.seasar.doma.jdbc.entity.PreDeleteContext;
+import org.seasar.doma.jdbc.entity.PreInsertContext;
+import org.seasar.doma.jdbc.entity.PreUpdateContext;
 
 public class EmpListener implements EntityListener<Emp> {
 
     @Override
-    public void preDelete(Emp entity) {
+    public void preDelete(Emp entity, PreDeleteContext context) {
     }
 
     @Override
-    public void preInsert(Emp entity) {
+    public void preInsert(Emp entity, PreInsertContext context) {
         entity.setInsertTimestamp(new Timestamp(new Date().getTime()));
     }
 
     @Override
-    public void preUpdate(Emp entity) {
-        entity.setUpdateTimestamp(new Timestamp(new Date().getTime()));
+    public void preUpdate(Emp entity, PreUpdateContext context) {
+        if (context.isEntityChanged()) {
+            entity.setUpdateTimestamp(new Timestamp(new Date().getTime()));
+        }
+    }
+
+    @Override
+    public void postInsert(Emp entity, PostInsertContext context) {
+    }
+
+    @Override
+    public void postUpdate(Emp entity, PostUpdateContext context) {
+    }
+
+    @Override
+    public void postDelete(Emp entity, PostDeleteContext context) {
     }
 
 }
