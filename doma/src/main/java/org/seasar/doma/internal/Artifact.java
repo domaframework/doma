566b35003f8738116ac2f6b510443fa780777fd7
nakamura-to <toshihiro.nakamura@gmail.com>
2011-02-04 22:43:02 +0000

    1.13.0リリース準備。
    git-svn-id: https://www.seasar.org/svn/doma/trunk@965 362887e2-e868-44d1-a061-b03550a0b845

--- doma/src/main/java/org/seasar/doma/internal/Artifact.java
+++ doma/src/main/java/org/seasar/doma/internal/Artifact.java
@@ -26,7 +26,7 @@ public final class Artifact {
 
     private static final String NAME = "Doma";
 
-    private static final String VERSION = "1.13.0-SNAPSHOT";
+    private static final String VERSION = "1.13.0";
 
     public static String getName() {
         return NAME;
