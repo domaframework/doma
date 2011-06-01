710164304c155752a9007fad577e353f51f86c7e
nakamura-to <toshihiro.nakamura@gmail.com>
2011-06-01 16:24:15 +0000

    バージョンを1.15.0-SNAPSHOTに変更しました。
    git-svn-id: https://www.seasar.org/svn/doma/trunk@994 362887e2-e868-44d1-a061-b03550a0b845

--- doma/src/main/java/org/seasar/doma/internal/Artifact.java
+++ doma/src/main/java/org/seasar/doma/internal/Artifact.java
@@ -26,7 +26,7 @@ public final class Artifact {
 
     private static final String NAME = "Doma";
 
-    private static final String VERSION = "1.14.1-SNAPSHOT";
+    private static final String VERSION = "1.15.0-SNAPSHOT";
 
     public static String getName() {
         return NAME;
