6774b394c021f5ec3321954063f9f2d5b1ff5a72
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2014-06-07 17:45:22 +0900

    2.0.0 dev start

--- docs/sources/build.rst
+++ docs/sources/build.rst
@@ -82,7 +82,7 @@ Gradle でビルドを行う際のポイントは以下のとおりです。
   }
 
   dependencies {
-      compile "org.seasar.doma:doma:2.0-beta-5"
+      compile "org.seasar.doma:doma:2.0.0-SNAPSHOT"
   }
 
 .. note::
--- src/main/java/org/seasar/doma/internal/Artifact.java
+++ src/main/java/org/seasar/doma/internal/Artifact.java
@@ -26,7 +26,7 @@ public final class Artifact {
 
     private static final String NAME = "Doma";
 
-    private static final String VERSION = "2.0-beta-5";
+    private static final String VERSION = "2.0.0-SNAPSHOT";
 
     public static String getName() {
         return NAME;
