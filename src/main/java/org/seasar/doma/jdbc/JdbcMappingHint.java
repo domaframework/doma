f27e502e0146080015f7e84831a30676e4cd9ba2
nakamura-to <toshihiro.nakamura@gmail.com>
2015-08-11 13:49:22 +0900

    JdbcMappingHint にJavaDocコメントを追加

--- src/main/java/org/seasar/doma/jdbc/JdbcMappingHint.java
+++ src/main/java/org/seasar/doma/jdbc/JdbcMappingHint.java
@@ -18,10 +18,24 @@ package org.seasar.doma.jdbc;
 import java.util.Optional;
 
 /**
+ * マッピングに関するヒント情報です。
+ * <p>
+ * {@link JdbcMappingVisitor} の実装クラスにおいて、次のマッピングのカスタマイズに利用できます。
+ * 
+ * <ul>
+ * <li>JavaのクラスからSQLの型へのマッピング（JavaからSQLのバインド変数へのマッピング）</li>
+ * <li>SQLの型からJavaのクラスへのマッピング（SQLの結果セットからJavaのマッピング）</li>
+ * </ul>
+ * 
  * @author nakamura-to
  * 
  */
 public interface JdbcMappingHint {
 
+    /**
+     * ドメインクラスにマッピングされている場合、そのドメインクラスを返します。
+     * 
+     * @return ドメインクラス
+     */
     Optional<Class<?>> getDomainClass();
 }
