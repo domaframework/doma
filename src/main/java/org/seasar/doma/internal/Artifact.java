187db3892eadcd2ed5ef0536a3d4a0faed212a7f
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2015-11-01 10:23:10 +0900

    Next development version [ci skip]

--- src/main/java/org/seasar/doma/internal/Artifact.java
+++ src/main/java/org/seasar/doma/internal/Artifact.java
@@ -26,7 +26,7 @@ public final class Artifact {
 
     private static final String NAME = "Doma";
 
-    private static final String VERSION = "2.5.1";
+    private static final String VERSION = "2.5.2-SNAPSHOT";
 
     public static String getName() {
         return NAME;
