13afdc43e09558581d0cd1044a6a3a2caf89aaf3
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2016-01-11 12:03:42 +0900

    2.6.1 released

--- src/main/java/org/seasar/doma/internal/Artifact.java
+++ src/main/java/org/seasar/doma/internal/Artifact.java
@@ -26,7 +26,7 @@ public final class Artifact {
 
     private static final String NAME = "Doma";
 
-    private static final String VERSION = "2.6.1-SNAPSHOT";
+    private static final String VERSION = "2.6.1";
 
     public static String getName() {
         return NAME;
