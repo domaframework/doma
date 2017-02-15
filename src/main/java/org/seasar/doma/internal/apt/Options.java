bb273518ee7a355decb7fa79289a43dd097cc08e
kakusuke <bananafishmonger+github@gmail.com>
2017-02-15 14:48:25 +0900

    設定ファイルから注釈処理のオプションを読み込めるように

--- src/main/java/org/seasar/doma/internal/apt/Options.java
+++ src/main/java/org/seasar/doma/internal/apt/Options.java
@@ -15,9 +15,19 @@
  */
 package org.seasar.doma.internal.apt;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
+import javax.annotation.processing.Filer;
 import javax.annotation.processing.ProcessingEnvironment;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
 
 import org.seasar.doma.internal.Artifact;
 
@@ -47,12 +57,14 @@ public final class Options {
 
     public static final String VERSION_VALIDATION = "doma.version.validation";
 
+    public static final String CONFIG_PATH = "doma.config.path";
+
     public static final String LOMBOK_ALL_ARGS_CONSTRUCTOR = "doma.lombok.AllArgsConstructor";
 
     public static final String LOMBOK_VALUE = "doma.lombok.Value";
 
     public static boolean isTestEnabled(ProcessingEnvironment env) {
-        String test = env.getOptions().get(Options.TEST);
+        String test = getOption(env, Options.TEST);
         return Boolean.valueOf(test).booleanValue();
     }
 
@@ -71,27 +83,27 @@ public final class Options {
     }
 
     public static boolean isDebugEnabled(ProcessingEnvironment env) {
-        String debug = env.getOptions().get(Options.DEBUG);
+        String debug = getOption(env, Options.DEBUG);
         return Boolean.valueOf(debug).booleanValue();
     }
 
     public static String getDaoPackage(ProcessingEnvironment env) {
-        String pkg = env.getOptions().get(Options.DAO_PACKAGE);
+        String pkg = getOption(env, Options.DAO_PACKAGE);
         return pkg != null ? pkg : null;
     }
 
     public static String getDaoSubpackage(ProcessingEnvironment env) {
-        String subpackage = env.getOptions().get(Options.DAO_SUBPACKAGE);
+        String subpackage = getOption(env, Options.DAO_SUBPACKAGE);
         return subpackage != null ? subpackage : null;
     }
 
     public static String getDaoSuffix(ProcessingEnvironment env) {
-        String suffix = env.getOptions().get(Options.DAO_SUFFIX);
+        String suffix = getOption(env, Options.DAO_SUFFIX);
         return suffix != null ? suffix : Constants.DEFAULT_DAO_SUFFIX;
     }
 
     public static String getEntityFieldPrefix(ProcessingEnvironment env) {
-        String prefix = env.getOptions().get(Options.ENTITY_FIELD_PREFIX);
+        String prefix = getOption(env, Options.ENTITY_FIELD_PREFIX);
         if ("none".equalsIgnoreCase(prefix)) {
             return "";
         }
@@ -99,33 +111,82 @@ public final class Options {
     }
 
     public static String getExprFunctions(ProcessingEnvironment env) {
-        String name = env.getOptions().get(Options.EXPR_FUNCTIONS);
+        String name = getOption(env, Options.EXPR_FUNCTIONS);
         return name != null ? name : null;
     }
 
     public static String getDomainConverters(ProcessingEnvironment env) {
-        String converters = env.getOptions().get(Options.DOMAIN_CONVERTERS);
+        String converters = getOption(env, Options.DOMAIN_CONVERTERS);
         return converters != null ? converters : null;
     }
 
     public static boolean getSqlValidation(ProcessingEnvironment env) {
-        String v = env.getOptions().get(Options.SQL_VALIDATION);
+        String v = getOption(env, Options.SQL_VALIDATION);
         return v != null ? Boolean.valueOf(v).booleanValue() : true;
     }
 
     public static boolean getVersionValidation(ProcessingEnvironment env) {
-        String v = env.getOptions().get(Options.VERSION_VALIDATION);
+        String v = getOption(env, Options.VERSION_VALIDATION);
         return v != null ? Boolean.valueOf(v).booleanValue() : true;
     }
 
+    public static String getConfigPath(ProcessingEnvironment env) {
+        String configPath = env.getOptions().get(Options.CONFIG_PATH);
+        return configPath != null ? configPath : Constants.DEFAULT_CONFIG_PATH;
+    }
+
     public static String getLombokAllArgsConstructor(ProcessingEnvironment env) {
-        String name = env.getOptions().get(Options.LOMBOK_ALL_ARGS_CONSTRUCTOR);
+        String name = getOption(env, Options.LOMBOK_ALL_ARGS_CONSTRUCTOR);
         return name != null ? name : Constants.DEFAULT_LOMBOK_ALL_ARGS_CONSTRUCTOR;
     }
 
     public static String getLombokValue(ProcessingEnvironment env) {
-        String name = env.getOptions().get(Options.LOMBOK_VALUE);
+        String name = getOption(env, Options.LOMBOK_VALUE);
         return name != null ? name : Constants.DEFAULT_LOMBOK_VALUE;
+    }
+
+    private static String getOption(ProcessingEnvironment env, String key) {
+        String v = env.getOptions().get(key);
+        if (v != null) {
+            return v;
+        }
+
+        return getConfig(env).get(key);
+    }
+
+    private static Map<String, Map<String, String>> configCache = new HashMap<>();
+    private static Map<String, String> getConfig(ProcessingEnvironment env) {
+        FileObject config = getFileObject(env, "", getConfigPath(env));
+        if (config == null) {
+            return Collections.emptyMap();
+        }
+        return configCache.computeIfAbsent(config.toUri().getPath(), configPath -> {
+            try {
+                return loadProperties(config);
+            } catch (IOException e) {
+                return Collections.emptyMap();
+            }
+        });
+    }
+
+    public static FileObject getFileObject(ProcessingEnvironment env, String pkg, String relativeName) {
+        Filer filer = env.getFiler();
+
+        try {
+            return filer.getResource(StandardLocation.CLASS_OUTPUT, pkg, relativeName);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Map<String, String> loadProperties(FileObject config) throws IOException {
+        try (InputStream is = config.openInputStream();
+             InputStreamReader isr = new InputStreamReader(is, "UTF-8")){
+            Properties props = new Properties();
+            props.load(isr);
+            return (Map<String, String>) new HashMap(props);
+        }
     }
 
     protected static class Constants {
@@ -133,6 +194,8 @@ public final class Options {
         public static final String DEFAULT_DAO_SUFFIX = "Impl";
 
         public static final String DEFAULT_ENTITY_FIELD_PREFIX = "$";
+
+        public static final String DEFAULT_CONFIG_PATH = "doma.config";
 
         public static final String DEFAULT_LOMBOK_ALL_ARGS_CONSTRUCTOR = "lombok.AllArgsConstructor";
 
