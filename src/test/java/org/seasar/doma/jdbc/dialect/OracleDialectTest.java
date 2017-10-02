f1649ce773a4cc4796971bccdb6c9d9079bb35f3
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2017-10-02 21:36:05 +0900

    Remove full-width `％` and `＿` from wild cards in Oracle dialect

--- src/test/java/org/seasar/doma/jdbc/dialect/OracleDialectTest.java
+++ src/test/java/org/seasar/doma/jdbc/dialect/OracleDialectTest.java
@@ -15,20 +15,9 @@
  */
 package org.seasar.doma.jdbc.dialect;
 
-import java.sql.Date;
-import java.sql.Time;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
+import org.seasar.doma.expr.ExpressionFunctions;
 
 import junit.framework.TestCase;
-
-import org.seasar.doma.expr.ExpressionFunctions;
-import org.seasar.doma.internal.jdbc.sql.ConvertToLogFormatFunction;
-import org.seasar.doma.jdbc.SqlLogFormattingVisitor;
-import org.seasar.doma.wrapper.DateWrapper;
-import org.seasar.doma.wrapper.TimeWrapper;
-import org.seasar.doma.wrapper.TimestampWrapper;
-import org.seasar.doma.wrapper.UtilDateWrapper;
 
 /**
  * @author taedium
@@ -39,54 +28,21 @@ public class OracleDialectTest extends TestCase {
     public void testExpressionFunctions_prefix() throws Exception {
         OracleDialect dialect = new OracleDialect();
         ExpressionFunctions functions = dialect.getExpressionFunctions();
-        assertEquals("a$$a$%a$_a$％a$＿%", functions.prefix("a$a%a_a％a＿"));
+        assertEquals("a$$a$%a$_a％a＿%", functions.prefix("a$a%a_a％a＿"));
     }
 
     public void testExpressionFunctions_prefix_escape() throws Exception {
         OracleDialect dialect = new OracleDialect();
         ExpressionFunctions functions = dialect.getExpressionFunctions();
-        assertEquals("a!!a!%a!_a!％a!＿%", functions.prefix("a!a%a_a％a＿", '!'));
+        assertEquals("a!!a!%a!_a％a＿%", functions.prefix("a!a%a_a％a＿", '!'));
     }
 
     public void testExpressionFunctions_prefix_escapeWithBackslash()
             throws Exception {
         OracleDialect dialect = new OracleDialect();
         ExpressionFunctions functions = dialect.getExpressionFunctions();
-        assertEquals("a\\\\a\\%a\\_a\\％a\\＿%",
+        assertEquals("a\\\\a\\%a\\_a％a＿%",
                 functions.prefix("a\\a%a_a％a＿", '\\'));
     }
 
-    public void testDateFormat() throws Exception {
-        OracleDialect dialect = new OracleDialect();
-        SqlLogFormattingVisitor visitor = dialect.getSqlLogFormattingVisitor();
-        DateWrapper wrapper = new DateWrapper(Date.valueOf("2009-01-23"));
-        assertEquals("date'2009-01-23'",
-                wrapper.accept(visitor, new ConvertToLogFormatFunction(), null));
-    }
-
-    public void testTimeFormat() throws Exception {
-        OracleDialect dialect = new OracleDialect();
-        SqlLogFormattingVisitor visitor = dialect.getSqlLogFormattingVisitor();
-        TimeWrapper wrapper = new TimeWrapper(Time.valueOf("01:23:45"));
-        assertEquals("time'01:23:45'",
-                wrapper.accept(visitor, new ConvertToLogFormatFunction(), null));
-    }
-
-    public void testTimestampFormat() throws Exception {
-        OracleDialect dialect = new OracleDialect();
-        SqlLogFormattingVisitor visitor = dialect.getSqlLogFormattingVisitor();
-        TimestampWrapper wrapper = new TimestampWrapper(
-                Timestamp.valueOf("2009-01-23 01:23:45.123456789"));
-        assertEquals("timestamp'2009-01-23 01:23:45.123456789'",
-                wrapper.accept(visitor, new ConvertToLogFormatFunction(), null));
-    }
-
-    public void testUtilDateFormat() throws Exception {
-        OracleDialect dialect = new OracleDialect();
-        SqlLogFormattingVisitor visitor = dialect.getSqlLogFormattingVisitor();
-        UtilDateWrapper wrapper = new UtilDateWrapper(new SimpleDateFormat(
-                "yyyy-MM-dd HH:mm:sss.SSS").parse("2009-01-23 12:34:56.789"));
-        assertEquals("timestamp'2009-01-23 12:34:56.789'",
-                wrapper.accept(visitor, new ConvertToLogFormatFunction(), null));
-    }
 }
