c4370c7b84265e9494c9ab0773345c1bd5cb529b
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2018-03-18 16:51:34 +0900

    Next development version [ci skip]

--- docs/sources/build.rst
+++ docs/sources/build.rst
@@ -82,7 +82,7 @@ Gradle でビルドを行う際のポイントは以下のとおりです。
   }
 
   dependencies {
-      compile "org.seasar.doma:doma:2.19.3"
+      compile "org.seasar.doma:doma:2.19.4-SNAPSHOT"
   }
 
 .. note::
--- src/main/java/org/seasar/doma/internal/Artifact.java
+++ src/main/java/org/seasar/doma/internal/Artifact.java
@@ -26,7 +26,7 @@ public final class Artifact {
 
     private static final String NAME = "Doma";
 
-    private static final String VERSION = "2.19.3";
+    private static final String VERSION = "2.19.4-SNAPSHOT";
 
     public static String getName() {
         return NAME;
