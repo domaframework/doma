d22d65348a5497ee2efc0bd1b08952ea20bb2254
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2020-04-17 21:56:04 +0900

    Release 2.30.0-beta-3

--- doma-core/src/main/java/org/seasar/doma/internal/Artifact.java
+++ doma-core/src/main/java/org/seasar/doma/internal/Artifact.java
@@ -7,7 +7,7 @@ public final class Artifact {
 
   private static final String NAME = "Doma";
 
-  private static final String VERSION = "2.30.0-beta-2";
+  private static final String VERSION = "2.30.0-beta-3";
 
   public static String getName() {
     return NAME;
