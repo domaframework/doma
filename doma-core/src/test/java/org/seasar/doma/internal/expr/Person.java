a067e6e7d97ebf8d79cc6adeb15734e66589b3ea
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2020-09-19 14:10:56 +0900

    Suppress SameReturnValue

--- doma-core/src/test/java/org/seasar/doma/internal/expr/Person.java
+++ doma-core/src/test/java/org/seasar/doma/internal/expr/Person.java
@@ -20,6 +20,7 @@ public class Person {
     return optionalName;
   }
 
+  @SuppressWarnings("SameReturnValue")
   public static Optional<String> getStaticOptionalName() {
     return staticOptionalName;
   }
