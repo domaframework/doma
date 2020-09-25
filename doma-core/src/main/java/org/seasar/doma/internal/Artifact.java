a65b3a9ea944e4027b79d3ec1ae4a98751b34fcd
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2020-09-25 20:36:44 +0900

    [Gradle Release Plugin] - [skip ci] new version commit:  '2.43.1-SNAPSHOT'.

--- doma-core/src/main/java/org/seasar/doma/internal/Artifact.java
+++ doma-core/src/main/java/org/seasar/doma/internal/Artifact.java
@@ -7,7 +7,7 @@ public final class Artifact {
 
   private static final String NAME = "Doma";
 
-  private static final String VERSION = "2.43.0";
+  private static final String VERSION = "2.43.1-SNAPSHOT";
 
   public static String getName() {
     return NAME;
