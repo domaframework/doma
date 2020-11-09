f63e5c1bb58db0b89791f1b0862824b8b8642894
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2020-11-09 21:41:16 +0900

    Consider the trailing semi-colon in SQL transformation

--- doma-core/src/test/java/org/seasar/doma/internal/jdbc/dialect/H2PagingTransformerTest.java
+++ doma-core/src/test/java/org/seasar/doma/internal/jdbc/dialect/H2PagingTransformerTest.java
@@ -26,10 +26,34 @@ public class H2PagingTransformerTest {
   }
 
   @Test
+  public void testOffsetLimit_semicolon() {
+    String expected = "select * from emp order by emp.id limit 10 offset 5;";
+    H2PagingTransformer transformer = new H2PagingTransformer(5, 10);
+    SqlParser parser = new SqlParser("select * from emp order by emp.id;");
+    SqlNode sqlNode = transformer.transform(parser.parse());
+    NodePreparedSqlBuilder sqlBuilder =
+        new NodePreparedSqlBuilder(new MockConfig(), SqlKind.SELECT, "dummyPath");
+    PreparedSql sql = sqlBuilder.build(sqlNode, Function.identity());
+    assertEquals(expected, sql.getRawSql());
+  }
+
+  @Test
   public void testOffsetLimit_forUpdate() {
     String expected = "select * from emp order by emp.id  limit 10 offset 5 for update";
     H2PagingTransformer transformer = new H2PagingTransformer(5, 10);
     SqlParser parser = new SqlParser("select * from emp order by emp.id for update");
+    SqlNode sqlNode = transformer.transform(parser.parse());
+    NodePreparedSqlBuilder sqlBuilder =
+        new NodePreparedSqlBuilder(new MockConfig(), SqlKind.SELECT, "dummyPath");
+    PreparedSql sql = sqlBuilder.build(sqlNode, Function.identity());
+    assertEquals(expected, sql.getRawSql());
+  }
+
+  @Test
+  public void testOffsetLimit_forUpdate_semicolon() {
+    String expected = "select * from emp order by emp.id  limit 10 offset 5 for update;";
+    H2PagingTransformer transformer = new H2PagingTransformer(5, 10);
+    SqlParser parser = new SqlParser("select * from emp order by emp.id for update;");
     SqlNode sqlNode = transformer.transform(parser.parse());
     NodePreparedSqlBuilder sqlBuilder =
         new NodePreparedSqlBuilder(new MockConfig(), SqlKind.SELECT, "dummyPath");
