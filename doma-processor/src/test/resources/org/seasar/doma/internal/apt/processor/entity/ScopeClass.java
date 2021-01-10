646de7186bddcd7aa5d22bdd0011e1b083c00478
orekyuu <orekyuu@gmail.com>
2021-01-10 14:24:47 +0900

    ScopeClassにscopeメソッドを追加

--- doma-processor/src/test/resources/org/seasar/doma/internal/apt/processor/entity/ScopeClass.java
+++ doma-processor/src/test/resources/org/seasar/doma/internal/apt/processor/entity/ScopeClass.java
@@ -1,4 +1,13 @@
 package org.seasar.doma.internal.apt.processor.entity;
 
+import org.seasar.doma.jdbc.criteria.declaration.WhereDeclaration;
+import java.util.function.Consumer;
+
 class ScopeClass {
+
+    public Consumer<WhereDeclaration> searchByName(ScopedEntity_ e, String text) {
+        return w -> {
+            w.like(e.name, "%" + text + "%");
+        };
+    }
 }
