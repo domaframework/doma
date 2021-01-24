3bbf63ea05ef33dcd04e2c57d0ca167d3a93d51e
orekyuu <orekyuu@gmail.com>
2021-01-25 00:04:59 +0900

    Add newest method to Publishable.

--- doma-processor/src/test/resources/org/seasar/doma/internal/apt/processor/entity/Publishable.java
+++ doma-processor/src/test/resources/org/seasar/doma/internal/apt/processor/entity/Publishable.java
@@ -1,6 +1,7 @@
 package org.seasar.doma.internal.apt.processor.entity;
 
 import org.seasar.doma.ScopeMethod;
+import org.seasar.doma.jdbc.criteria.declaration.OrderByNameDeclaration;
 import org.seasar.doma.jdbc.criteria.declaration.WhereDeclaration;
 
 import java.time.LocalDateTime;
@@ -17,5 +18,10 @@ interface Publishable<E extends EntityMetamodel<?>> {
         return w -> {
             w.le(publishedAt(e), LocalDateTime.now());
         };
+    }
+
+    @ScopeMethod
+    default Consumer<OrderByNameDeclaration> newest(E e) {
+        return o -> o.desc(publishedAt(e));
     }
 }
