9e2862ca49a5e169942b8a99c721c0a819e3befd
orekyuu <orekyuu@gmail.com>
2021-01-26 21:03:15 +0900

    Add generic class test.

--- doma-processor/src/test/resources/org/seasar/doma/internal/apt/processor/entity/ItemScope.java
+++ doma-processor/src/test/resources/org/seasar/doma/internal/apt/processor/entity/ItemScope.java
@@ -6,9 +6,13 @@ import org.seasar.doma.jdbc.criteria.metamodel.PropertyMetamodel;
 import java.time.LocalDateTime;
 import java.util.function.Consumer;
 
-class ItemScope implements Publishable<ItemEntity_> {
+class ItemScope implements Publishable<ItemEntity_>, GenericIdQueries<ItemEntity_, Long> {
 
     public PropertyMetamodel<LocalDateTime> publishedAt(ItemEntity_ e) {
         return e.publishedAt;
+    }
+
+    public PropertyMetamodel<Long> id(ItemEntity_ e) {
+        return e.id;
     }
 }
