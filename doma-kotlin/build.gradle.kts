c29b97af5ce7419d715b13a54828b0bcaf04e323
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-10-31 22:46:14 +0900

    Don't set kotlinOptions.jvmTarget explicitly.
    Because Java toolchains does it.
    https://kotlinlang.org/docs/whatsnew1530.html#support-for-java-toolchains

--- doma-kotlin/build.gradle.kts
+++ doma-kotlin/build.gradle.kts
@@ -8,14 +8,3 @@ dependencies {
     api(project(":doma-core"))
     testImplementation(project(":doma-mock"))
 }
-
-tasks {
-    val jvmTarget = "1.8"
-
-    compileKotlin {
-        kotlinOptions.jvmTarget = jvmTarget
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = jvmTarget
-    }
-}
--- integration-test-kotlin/build.gradle.kts
+++ integration-test-kotlin/build.gradle.kts
@@ -16,9 +16,3 @@ dependencies {
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(javaLangVersion))
 }
-
-tasks {
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = if (javaLangVersion <= 8) "1.8" else javaLangVersion.toString()
-    }
-}
