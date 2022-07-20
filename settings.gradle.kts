ca518d33e0bc0d77997d4041874b7b171f4eaf55
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2022-07-20 21:16:54 +0900

    Replace "org.seasar.doma.compile" with "org.domaframework.doma.compile" (#863)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
         id("com.diffplug.spotless") version "6.7.2"
         id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
         id("net.researchgate.release") version "3.0.0"
-        id("org.seasar.doma.compile") version "1.1.0"
+        id("org.domaframework.doma.compile") version "2.0.0"
         kotlin("jvm") version "1.6.21"
         kotlin("kapt") version "1.6.21"
     }
