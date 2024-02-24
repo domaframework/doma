cc57b1d403cceabc404b72e13ee69eb39387d373
t.momose <hyakkun@gmail.com>
2024-02-24 19:46:00 +0900

    Revert redundant Salary fix

--- integration-test-java/src/main/java/org/seasar/doma/it/criteria/Salary.java
+++ integration-test-java/src/main/java/org/seasar/doma/it/criteria/Salary.java
@@ -1,7 +1,6 @@
 package org.seasar.doma.it.criteria;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 import org.seasar.doma.Domain;
 
 @Domain(valueType = BigDecimal.class)
@@ -24,18 +23,5 @@ public class Salary {
   @Override
   public String toString() {
     return value.toString();
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Salary salary = (Salary) o;
-    return Objects.equals(value, salary.value);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(value);
   }
 }
