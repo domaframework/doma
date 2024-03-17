26c55c007278ce521a7b679cd59758ee529b349e
t.momose <hyakkun@gmail.com>
2024-03-17 23:26:35 +0900

    Implements Db2UpsertAssembler

--- doma-core/src/main/java/org/seasar/doma/jdbc/dialect/Db2Dialect.java
+++ doma-core/src/main/java/org/seasar/doma/jdbc/dialect/Db2Dialect.java
@@ -21,6 +21,8 @@ import org.seasar.doma.jdbc.criteria.metamodel.PropertyMetamodel;
 import org.seasar.doma.jdbc.criteria.option.ForUpdateOption;
 import org.seasar.doma.jdbc.criteria.query.AliasManager;
 import org.seasar.doma.jdbc.criteria.query.CriteriaBuilder;
+import org.seasar.doma.jdbc.query.UpsertAssembler;
+import org.seasar.doma.jdbc.query.UpsertAssemblerContext;
 
 /** A dialect for Db2. */
 public class Db2Dialect extends StandardDialect {
@@ -194,5 +196,10 @@ public class Db2Dialect extends StandardDialect {
             }
           });
     }
+  }
+
+  @Override
+  public UpsertAssembler getUpsertAssembler(UpsertAssemblerContext context) {
+    return new Db2UpsertAssembler(context);
   }
 }
