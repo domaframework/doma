0ba7a81934497d20d6b9205a5ca9f994cf6190e9
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2024-08-31 18:43:03 +0900

    Fix issue with duplicate log output in the integration test projects

--- integration-test-common/src/main/java/org/seasar/doma/it/LogConfigurator.java
+++ integration-test-common/src/main/java/org/seasar/doma/it/LogConfigurator.java
@@ -1,20 +1,25 @@
 package org.seasar.doma.it;
 
+import static ch.qos.logback.classic.spi.Configurator.ExecutionStatus.DO_NOT_INVOKE_NEXT_IF_ANY;
+import static ch.qos.logback.classic.spi.Configurator.ExecutionStatus.INVOKE_NEXT_IF_ANY;
+
 import ch.qos.logback.classic.BasicConfigurator;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.spi.Configurator;
 
 public class LogConfigurator extends BasicConfigurator {
 
   @Override
-  public Configurator.ExecutionStatus configure(LoggerContext lc) {
-    super.configure(lc);
+  public ExecutionStatus configure(LoggerContext lc) {
+    var status = super.configure(lc);
     Logger logger = lc.getLogger("org.seasar.doma.jdbc.LogKind.SQL");
     logger.setLevel(Level.DEBUG);
     Logger rootLogger = lc.getLogger(Logger.ROOT_LOGGER_NAME);
     rootLogger.setLevel(Level.INFO);
-    return Configurator.ExecutionStatus.NEUTRAL;
+    return switch (status) {
+      case NEUTRAL -> DO_NOT_INVOKE_NEXT_IF_ANY;
+      case INVOKE_NEXT_IF_ANY, DO_NOT_INVOKE_NEXT_IF_ANY -> status;
+    };
   }
 }
