01fb54f2207455ee9eb69b1957a273b328f4d5c6
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2024-12-28 08:55:43 +0900

    Use ECJ for building integration tests

--- integration-test-java/build.gradle.kts
+++ integration-test-java/build.gradle.kts
@@ -1,15 +1,62 @@
 plugins {
     java
+    alias(libs.plugins.themrmilchmann.ecj) apply false
 }
+
+val compiler: String by project
 
 dependencies {
     annotationProcessor(project(":doma-processor"))
     implementation(project(":doma-core"))
     testImplementation(project(":integration-test-common"))
+    if (compiler == "ecj") {
+        implementation(libs.ecj)
+        // See https://github.com/TheMrMilchmann/gradle-ecj/issues/30
+        compileOnly(project(":doma-processor"))
+    }
 }
 
-tasks {
-    compileJava {
-        options.compilerArgs.addAll(listOf("-Adoma.domain.converters=org.seasar.doma.it.domain.DomainConverterProvider"))
+when(compiler) {
+    "javac" -> {
+        tasks {
+            compileJava {
+                options.compilerArgs.addAll(
+                    listOf(
+                        "-Adoma.domain.converters=org.seasar.doma.it.domain.DomainConverterProvider",
+                        "-Adoma.stats=true",
+                    )
+                )
+            }
+        }
     }
+    "ecj" -> {
+        apply(plugin = libs.plugins.themrmilchmann.ecj.get().pluginId)
+        val processors = listOf(
+            "org.seasar.doma.internal.apt.processor.DomainProcessor",
+            "org.seasar.doma.internal.apt.processor.DataTypeProcessor",
+            "org.seasar.doma.internal.apt.processor.ExternalDomainProcessor",
+            "org.seasar.doma.internal.apt.processor.DomainConvertersProcessor",
+            "org.seasar.doma.internal.apt.processor.EmbeddableProcessor",
+            "org.seasar.doma.internal.apt.processor.EntityProcessor",
+            "org.seasar.doma.internal.apt.processor.DaoProcessor",
+            "org.seasar.doma.internal.apt.processor.ScopeProcessor",
+        )
+        tasks {
+            compileJava {
+                @Suppress("UnstableApiUsage")
+                options.forkOptions.jvmArgumentProviders.add(
+                    CommandLineArgumentProvider {
+                        listOf(
+                            // The processors are not automatically detected, so it must be explicitly specified.
+                            "-processor",
+                            processors.joinToString(","),
+                            "-Adoma.domain.converters=org.seasar.doma.it.domain.DomainConverterProvider",
+                            "-Adoma.stats=true",
+                        )
+                    }
+                )
+            }
+        }
+    }
+    else -> error("Unknown compiler: $compiler")
 }
