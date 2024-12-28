dbfc7d6f87bf5be3b6d60eb6b9c1236a63e03dc8
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2024-12-28 10:37:17 +0900

    Handle the issue where ECJ invokes the processor multiple times

--- integration-test-java/build.gradle.kts
+++ integration-test-java/build.gradle.kts
@@ -16,47 +16,45 @@ dependencies {
     }
 }
 
-when(compiler) {
+val commonArgs = listOf(
+    "-Adoma.domain.converters=org.seasar.doma.it.domain.DomainConverterProvider",
+)
+
+// The processors are not automatically detected, so it must be explicitly specified.
+val ecjArgs = listOf(
+    "-processor",
+    listOf(
+        "org.seasar.doma.internal.apt.processor.DomainProcessor",
+        "org.seasar.doma.internal.apt.processor.DataTypeProcessor",
+        "org.seasar.doma.internal.apt.processor.ExternalDomainProcessor",
+        "org.seasar.doma.internal.apt.processor.DomainConvertersProcessor",
+        "org.seasar.doma.internal.apt.processor.EmbeddableProcessor",
+        "org.seasar.doma.internal.apt.processor.EntityProcessor",
+        "org.seasar.doma.internal.apt.processor.DaoProcessor",
+        "org.seasar.doma.internal.apt.processor.ScopeProcessor",
+    ).joinToString(","),
+)
+
+when (compiler) {
     "javac" -> {
         tasks {
             compileJava {
-                options.compilerArgs.addAll(
-                    listOf(
-                        "-Adoma.domain.converters=org.seasar.doma.it.domain.DomainConverterProvider",
-                        "-Adoma.stats=true",
-                    )
-                )
+                options.compilerArgs.addAll(commonArgs)
             }
         }
     }
+
     "ecj" -> {
         apply(plugin = libs.plugins.themrmilchmann.ecj.get().pluginId)
-        val processors = listOf(
-            "org.seasar.doma.internal.apt.processor.DomainProcessor",
-            "org.seasar.doma.internal.apt.processor.DataTypeProcessor",
-            "org.seasar.doma.internal.apt.processor.ExternalDomainProcessor",
-            "org.seasar.doma.internal.apt.processor.DomainConvertersProcessor",
-            "org.seasar.doma.internal.apt.processor.EmbeddableProcessor",
-            "org.seasar.doma.internal.apt.processor.EntityProcessor",
-            "org.seasar.doma.internal.apt.processor.DaoProcessor",
-            "org.seasar.doma.internal.apt.processor.ScopeProcessor",
-        )
         tasks {
             compileJava {
                 @Suppress("UnstableApiUsage")
                 options.forkOptions.jvmArgumentProviders.add(
-                    CommandLineArgumentProvider {
-                        listOf(
-                            // The processors are not automatically detected, so it must be explicitly specified.
-                            "-processor",
-                            processors.joinToString(","),
-                            "-Adoma.domain.converters=org.seasar.doma.it.domain.DomainConverterProvider",
-                            "-Adoma.stats=true",
-                        )
-                    }
+                    CommandLineArgumentProvider { commonArgs + ecjArgs }
                 )
             }
         }
     }
+
     else -> error("Unknown compiler: $compiler")
 }
